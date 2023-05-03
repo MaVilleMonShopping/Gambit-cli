9e64e81086c58cc72466d619d7dc5d6e748321d6
Arnaud DA SILVA <a.da-silva@mavillemonshopping.fr>
2023-05-03 17:43:25 +0200

    fix(firebase): verbose flag

--- bin/src/firebase/last_build_number/descriptor.dart
+++ bin/src/firebase/last_build_number/descriptor.dart
@@ -8,34 +8,36 @@ const String defaultNumberArgName = "default-number";
 class _LastBuildNumberDescriptor extends CommandDescriptor {
   _LastBuildNumberDescriptor()
       : super(
-          commandName: "last-build-number",
-          commandDescription:
-              "Get last build number from App Distribution releases.",
-          options: [
-            CommandOption(
-              name: serviceAccountFilePathArgName,
-              abbr: "f",
-              mandatory: true,
-              help: "Path to your .json service account file.",
-            ),
-            CommandOption(
-              name: projectIdArgName,
-              abbr: "p",
-              mandatory: true,
-              help: "Firebase project id.",
-            ),
-            CommandOption(
-              name: appIdArgName,
-              abbr: "a",
-              mandatory: true,
-              help: "Firebase app id.",
-            ),
-            CommandOption(
-              name: defaultNumberArgName,
-              abbr: "n",
-              mandatory: false,
-              help: "If no release found, will return this value.",
-            ),
-          ],
-        );
+            commandName: "last-build-number",
+            commandDescription:
+                "Get last build number from App Distribution releases.",
+            options: [
+              CommandOption(
+                name: serviceAccountFilePathArgName,
+                abbr: "f",
+                mandatory: true,
+                help: "Path to your .json service account file.",
+              ),
+              CommandOption(
+                name: projectIdArgName,
+                abbr: "p",
+                mandatory: true,
+                help: "Firebase project id.",
+              ),
+              CommandOption(
+                name: appIdArgName,
+                abbr: "a",
+                mandatory: true,
+                help: "Firebase app id.",
+              ),
+              CommandOption(
+                name: defaultNumberArgName,
+                abbr: "n",
+                mandatory: false,
+                help: "If no release found, will return this value.",
+              ),
+            ],
+            flags: [
+              verboseFlag()
+            ]);
 }
