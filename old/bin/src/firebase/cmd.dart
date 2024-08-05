de9a864408d65dada7767c546fb3ec698661a839
Danaru87 <danaru@tuta.io>
2024-08-05 14:37:43 +0200

    doc: update de la doc

--- old/bin/src/firebase/cmd.dart
+++
@@ -1,6 +0,0 @@
-import '../core/gambit_command.dart';
-import 'descriptor.dart';
-
-class FirebaseCMD extends GambitCommand {
-  FirebaseCMD() : super(FirebaseDescriptor());
-}
--- old/bin/src/ios/cmd.dart
+++
@@ -1,6 +0,0 @@
-import '../core/gambit_command.dart';
-import 'descriptor.dart';
-
-class IOSCommands extends GambitCommand {
-  IOSCommands() : super(IOSDescriptor());
-}
